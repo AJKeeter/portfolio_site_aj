c5e01e9f92b09e15a742d0365c5577f955262ec3
AJ Keeter <aleckeeter94@gmail.com>
2019-08-22 17:05:40 -0500

    Updated master layout styles along with helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,4 @@ class ApplicationController < ActionController::Base
   include CurrentUserConcern
   include DefaultPageContent
 
-  # before_action :set_title
-
-  # def set_title
-  #   @page_title = "My Portfolio Site | Alec Keeter's Website"
-  # end
-
 end
