4b736a7af73fb80fa0bfffb51f84c1907460d89b
AJ Keeter <aleckeeter94@gmail.com>
2019-08-30 14:38:43 -0500

    schema migration

--- db/migrate/20190818173649_create_technologies.rb
+++ db/migrate/20190818173649_create_technologies.rb
@@ -1,5 +1,7 @@
 class CreateTechnologies < ActiveRecord::Migration[5.2]
   def change
+
+    drop_table :technologies
     create_table :technologies do |t|
       t.string :name
       t.integer :portfolio_id
